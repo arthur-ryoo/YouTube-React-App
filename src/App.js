530733d18553b55e32c613c2277b8788d6ab437b
arthur-ryoo <jene0523@gmail.com>
2020-08-10 22:47:44 -0700

    Add grid and import search bar and video detail

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from 'react';
 
+import { Grid } from '@material-ui/core';
+
+import { SearchBar, VideoDetail } from './components/';
+// import { SearchBar, VideoList, VideoDetail } from './components';
+
+import youtube from './api/youtube';
+
 class App extends React.Component {
   render() {
-    return <h1>YouTube Clone App</h1>;
+    return (
+      <Grid justify="center" container spacing={16}>
+        <Grid item xs={12}>
+          <Grid container spacing={16}>
+            <Grid item xs={12}>
+              <SearchBar />
+            </Grid>
+            <Grid item xs={8}>
+              <VideoDetail />
+            </Grid>
+            <Grid item xs={4}>
+              {/* VIDEO LIST */}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    );
   }
 }
 
