da0501b6090375e1718fc4b8e8d9346df172e5aa
arthur-ryoo <jene0523@gmail.com>
2020-08-10 23:23:03 -0700

    Add handle submit and params for axios

--- src/App.js
+++ src/App.js
@@ -8,13 +8,25 @@ import { SearchBar, VideoDetail } from './components/';
 import youtube from './api/youtube';
 
 class App extends React.Component {
+  handleSubmit = async (searchTerm) => {
+    const response = await youtube.get('search', {
+      params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: process.env.REACT_APP_API_KEY,
+        q: searchTerm,
+      },
+    });
+    console.log(response.data.items);
+  };
+
   render() {
     return (
-      <Grid justify="center" container spacing={16}>
+      <Grid justify="center" container spacing={10}>
         <Grid item xs={12}>
-          <Grid container spacing={16}>
+          <Grid container spacing={10}>
             <Grid item xs={12}>
-              <SearchBar />
+              <SearchBar onFormSubmit={this.handleSubmit} />
             </Grid>
             <Grid item xs={8}>
               <VideoDetail />
