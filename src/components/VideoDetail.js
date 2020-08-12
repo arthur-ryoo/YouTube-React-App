1ebf7e4661c6a5d092bd4cf89c3615fcd4c6b700
arthur-ryoo <jene0523@gmail.com>
2020-08-11 23:44:43 -0700

    Fetch youtube api data for title and description

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -5,15 +5,29 @@ import { Paper, Typography } from '@material-ui/core';
 const VideoDetail = ({ video }) => {
   if (!video) return <div>Loading...</div>;
 
-  console.log(video.id.videoId);
+  console.log(video);
+
   const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
 
   return (
     <React.Fragment>
       <Paper elevation={6} style={{ height: '70%' }}>
-        <iframe frameBorder="0" height="100%" title="Video Player" src="" />
+        <iframe
+          frameBorder="0"
+          height="100%"
+          title="Video Player"
+          src={videoSrc}
+        />
       </Paper>
-      <Paper elevation={6} style={{ padding: '15px' }}></Paper>
+      <Paper elevation={6} style={{ padding: '15px' }}>
+        <Typography variant="h4">
+          {video.snippet.title} - {video.snippet.channelTitle}
+        </Typography>
+        <Typography variant="subtitle1">
+          {video.snippet.channelTitle}
+        </Typography>
+        <Typography variant="subtitle2">{video.snippet.description}</Typography>
+      </Paper>
     </React.Fragment>
   );
 };
