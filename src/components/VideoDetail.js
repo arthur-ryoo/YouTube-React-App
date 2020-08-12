feeeba99347baf1579d7d0838c12e0e85c30f783
arthur-ryoo <jene0523@gmail.com>
2020-08-11 23:36:42 -0700

    Add bolierplate for video detail component

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -3,7 +3,19 @@ import React from 'react';
 import { Paper, Typography } from '@material-ui/core';
 
 const VideoDetail = ({ video }) => {
-  return <h1>This is a video detail component</h1>;
+  if (!video) return <div>Loading...</div>;
+
+  console.log(video.id.videoId);
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
+  return (
+    <React.Fragment>
+      <Paper elevation={6} style={{ height: '70%' }}>
+        <iframe frameBorder="0" height="100%" title="Video Player" src="" />
+      </Paper>
+      <Paper elevation={6} style={{ padding: '15px' }}></Paper>
+    </React.Fragment>
+  );
 };
 
 export default VideoDetail;
