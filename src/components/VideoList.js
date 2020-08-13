5bc4a7d7bba46c29dd8074f429c98cab3a69a42a
arthur-ryoo <jene0523@gmail.com>
2020-08-12 22:34:39 -0700

    Create video list component

---
+++ src/components/VideoList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Grid } from '@material-ui/core';
+
+import VideoItem from './VideoItem';
+
+const VideoList = ({ videos }) => {
+  const listOfVideos = videos.map((video, index) => (
+    <VideoItem key={index} video={video} />
+  ));
+  return (
+    <Grid container spacing={10}>
+      {listOfVideos}
+    </Grid>
+  );
+};
+
+export default VideoList;
